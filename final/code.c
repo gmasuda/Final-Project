96b6fa9fd11b308ceceb57379414cb9cc4775f8b
gmasuda <143151714+gmasuda@users.noreply.github.com>
2023-12-12 15:56:05 -0800

    Update code.c

--- final/code.c
+++ final/code.c
@@ -2,13 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <dirent.h>
 #include <sys/stat.h>
+#include <dirent.h>
+
+void scanDirectory(const char *path);
+int txtToMap(const char *filename);
 
 #define SIZE_OF_MAP 10
 int map[SIZE_OF_MAP][SIZE_OF_MAP];
 
-int printMap();
+int main()
+{
+    const char *folderPath = "./Map";
+    scanDirectory(folderPath);
+
+    return 0;
+}
+
+void scanDirectory(const char *path)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if ((dir = opendir(path)) == NULL)
+    {
+        perror("opendir");
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL)
+    {
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+        {
+            continue;
+        }
+        char subPath[256];
+        snprintf(subPath, sizeof(subPath), "%s/%s", path, entry->d_name);
+
+        DIR *subDir = opendir(subPath);
+        if (subDir != NULL)
+        {
+            closedir(subDir);
+            scanDirectory(subPath);
+        }
+        else
+        {
+            const char *dot = strrchr(entry->d_name, '.');
+            if (dot != NULL && strcmp(dot, ".txt") == 0)
+            {
+                printf("Found text file: %s\n", subPath);
+                txtToMap(subPath);
+            }
+        }
+    }
+
+    closedir(dir);
+}
+
+int printMap()
+{
+    for (int i = 0; i < SIZE_OF_MAP; i++)
+    {
+        for (int j = 0; j < SIZE_OF_MAP; j++)
+        {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
 
 int *findNeighboringPoints(int x, int y)
 {
@@ -43,15 +105,12 @@ int positionToFolder(const char *filename)
 {
     char modifier = filename[strlen(filename) - 5];
     char folderName[256];
-    char folderName2[256];
     for (int i = 0; i < SIZE_OF_MAP; i++)
     {
         for (int j = 0; j < SIZE_OF_MAP; j++)
         {
             snprintf(folderName, sizeof(folderName), "/home/garrett_masuda9/Final-Project/final/World/%c%d%d", modifier, i, j);
-
-            // Use mkdir from sys/stat.h instead of _mkdir
-            if (mkdir(folderName, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0)
+            if (mkdir(folderName, 0777) != 0)
             {
                 perror("mkdir");
             }
@@ -94,26 +153,3 @@ int txtToMap(const char *filename)
     positionToFolder(filename);
     return 0;
 }
-
-int main()
-{
-    const char *folderPath = "./Map";
-    txtToMap(folderPath);
-
-    return 0;
-}
-
-int printMap()
-{
-    for (int i = 0; i < SIZE_OF_MAP; i++)
-    {
-        for (int j = 0; j < SIZE_OF_MAP; j++)
-        {
-            printf("%d ", map[i][j]);
-        }
-        printf("\n");
-    }
-    return 0;
-}
-
-
